69dcbeebf837aab3691cf93b524bd4e36a306739
Axita Kathiriya <axitakathiriya11@gmail.com>
2022-07-18 09:39:34 +0530

    #first-commit Files added

--- App.js
+++ App.js
@@ -1,1 +1,23 @@
+const reviewSlider = document.querySelector('.slides__track')
 
+let time = 0;
+let count = 1; 
+
+function slideAnimation(){
+    if(time % 270 === 0){
+        if(count === 3) count = 0
+        let c = count * 90;
+        let sliderPosition = `-${c}vw`
+        reviewSlider.style.transform = `translate(${sliderPosition})`
+        time = 0;
+        count++
+    }
+}
+
+function animate(){
+    time++
+    slideAnimation()
+    requestAnimationFrame(animate)
+}
+
+animate();
